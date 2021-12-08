4f20c992d77dd4f4ea56ad865de26a3e9acf5917
CWG <CWG@gmail.com>
2021-12-08 13:53:46 +0530

    get people by name

--- rough/src/lamdaExpressions/PersonsMain.java
+++ rough/src/lamdaExpressions/PersonsMain.java
@@ -16,11 +16,14 @@ public class PersonsMain {
         // create a method that prints all elements in list
         printAll(people);
         // print all names whose first name start with a
-        printAllFirstNameStartsWith(people, "");
+        printAllFirstNameStartsWith(people, "su");
     }
 
     static void printAllFirstNameStartsWith(List<Person> people, String str) {
-
+        for (Person person : people) {
+            if (person.getName().startsWith(str))
+                System.out.println(person.getName());
+        }
     }
 
     static void printAll(List<Person> people) {
