25b1ac99262733a338fa51f872f4fdc415d574b3
CWG <CWG@gmail.com>
2022-02-03 15:01:06 +0530

    adding map()

--- rough/src/stream/Product.java
+++ rough/src/stream/Product.java
@@ -16,4 +16,28 @@ public class Product {
         return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
 }
