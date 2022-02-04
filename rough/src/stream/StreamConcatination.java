d56c17b8fbd2df4a47887f0d6f8bc7ba866344c1
CWG <CWG@gmail.com>
2022-02-04 15:16:21 +0530

    System.currentTimeMills()

--- rough/src/stream/StreamConcatination.java
+++ rough/src/stream/StreamConcatination.java
@@ -7,12 +7,13 @@ import java.util.stream.Stream;
 
 public class StreamConcatination {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         List<String> animalList = Arrays.asList("Dog", "Cat", "Elephant");
         List<String> birdList = Arrays.asList("peacock", "parrot", "crow");
-
         Stream<String> s1 = animalList.stream();
         Stream<String> s2 = birdList.stream();
         List<String> fList = Stream.concat(s1, s2).collect(Collectors.toList());
         System.out.println(fList);
+        System.out.println(System.currentTimeMillis() - start);
     }
 }
